973dc5330aa6801fade3bb03f0f142e8885a8f26
Balazs Nyirfa <balazsnyirfa@gmail.com>
2020-07-14 12:16:48 +0200

    refactor: simplify Remove-EmptyDirectories using the ternary operator

--- Datapress.Remove-EmptyDirectories/Datapress.Remove-EmptyDirectories.psm1
+++ Datapress.Remove-EmptyDirectories/Datapress.Remove-EmptyDirectories.psm1
@@ -9,23 +9,13 @@ function Remove-EmptyDirectories {
     # get your current directory's child directories as an array
     $directoryList = Get-ChildItem -Directory
 
-    if ($Dryrun) {
-    # check if dryrun true or not, if so the code will run with the -WhatIf param to simulate the process
-        foreach ($directory in $directoryList) {
-            if ((Get-ChildItem $directory | Measure-Object).Count -eq 0) {
-                # if a directory contains no child item at all, that directory will be deleted
-                Remove-Item $directory -WhatIf
-            }
+    foreach ($directory in $directoryList) {
+        if ((Get-ChildItem $directory | Measure-Object).Count -eq 0) {
+            # if a directory contains no child item at all, that directory will be deleted
+            ($Dryrun) ? (Remove-Item $directory -WhatIf) : (Remove-Item $directory)
         }
-        Write-Host "Dry-run operation has completed." -ForegroundColor Green
     }
-    else {
-        foreach ($directory in $directoryList) {
-            if ((Get-ChildItem $directory | Measure-Object).Count -eq 0) {
-                # if a directory contains no child item at all, that directory will be deleted
-                Remove-Item $directory
-            }
-        }
-        Write-Host "Operation has completed." -ForegroundColor Green
-    }
+    
+    $completeMsg = ($Dryrun) ? "Dry-run operation has completed." : "Operation has completed."
+    Write-Host $completeMsg -ForegroundColor Green
 }
