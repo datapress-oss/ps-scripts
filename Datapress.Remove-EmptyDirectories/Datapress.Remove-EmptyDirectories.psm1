4b2e2b45ce9907cd6ae89baff884e38175bfa0bb
Balazs Nyirfa <balazsnyirfa@gmail.com>
2020-07-14 13:04:36 +0200

    docs: add Comment-based Help to Datapress.Remove-EmptyDirectories

--- Datapress.Remove-EmptyDirectories/Datapress.Remove-EmptyDirectories.psm1
+++ Datapress.Remove-EmptyDirectories/Datapress.Remove-EmptyDirectories.psm1
@@ -1,4 +1,37 @@
 function Remove-EmptyDirectories {
+<#
+.SYNOPSIS
+
+Finds empty directories and deletes them.
+
+.DESCRIPTION
+
+Finds empty directories and deletes them.
+If there is a directory with only hidden item/items in it, you'll be prompted to confirm the deletion!
+Your current directory "pwd" has to be the parent directory of the directory/s you want to be deleted.
+
+.PARAMETER Dryrun
+This parameter does not need a value. If provided, the deletion will be simulated with verbose output, but no actual changes will happen.
+
+.INPUTS
+
+None. You cannot pipe objects to Add-Extension.
+
+.EXAMPLE
+
+PS> Remove-EmptyDirectories
+Operation has completed!
+
+.EXAMPLE
+
+PS> Remove-EmptyDirectories -Dryrun
+What if: Performing the operation "Remove Directory" on target "C:\Users\username\ParentDir\child1".
+What if: Performing the operation "Remove Directory" on target "C:\Users\username\ParentDir\child2".
+What if: Performing the operation "Remove Directory" on target "C:\Users\username\ParentDir\child3".
+Dry-run operation has completed.
+
+#>
+
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $false)]
@@ -15,7 +48,7 @@ function Remove-EmptyDirectories {
             ($Dryrun) ? (Remove-Item $directory -WhatIf) : (Remove-Item $directory)
         }
     }
-    
+
     $completeMsg = ($Dryrun) ? "Dry-run operation has completed." : "Operation has completed."
     Write-Host $completeMsg -ForegroundColor Green
 }
